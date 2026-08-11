e37c7b733cfe6ee44c9cca5dbb4b34187a504cd6
erafw <schwabrafaell@gmail.com>
2026-08-11 17:46:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-#http://127.0.0.1:8000/teste1
-@app.get("/teste1")
+#http://127.0.0.1:8000/teste
+@app.get("/teste")
 async def funcaoteste():
     return {"teste": True, "num_aleatorio": random.randit(0,1000)}
